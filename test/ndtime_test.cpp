0a30fa5970883891b085e05ee53e3ff78c2387ba
Laouen <laouen.belloli@gmail.com>
2017-09-19 00:15:24 -0300

    using main-test.cpp as the tests main file. Using this and boost/test/unit_tests instead of boost/test/include/unit_tests speeds up the tests compilation time

--- test/ndtime_test.cpp
+++ test/ndtime_test.cpp
@@ -1,28 +1,28 @@
 /**
  * Copyright (C) 2016  Laouen Mayal Louan Belloli
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+#define BOOST_TEST_DYN_LINK
 #include "../NDTime.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
 #include <sstream>
-#include <string>
 
-#define BOOST_TEST_MODULE ndtime
-#include <boost/test/included/unit_test.hpp>
+#include <string>
+#include <boost/test/unit_test.hpp>
 
 
 BOOST_AUTO_TEST_SUITE( ndtime_constructors )
